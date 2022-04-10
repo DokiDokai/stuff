4fad80d68673a531c78b5ae9ad2bca84a6cf0e07
Dela <103084998+DokiDokai@users.noreply.github.com>
2022-04-10 09:48:17 -0700

    added anti-afk
    :)

--- stay-alive-and-steal-time.lua
+++ stay-alive-and-steal-time.lua
@@ -4,6 +4,14 @@ for i,v in pairs(game.Workspace:GetChildren()) do
     end
 end
 
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
+-----------------------------------
+
 
 local FarmBase = Instance.new("Part")
 

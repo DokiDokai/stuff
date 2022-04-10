b6fc9b8828edbfbdae8b4b040db628c1a0f2b459
Dela <103084998+DokiDokai@users.noreply.github.com>
2022-04-10 09:39:04 -0700

    Create stay-alive-and-steal-time.lua

---
+++ stay-alive-and-steal-time.lua
@@ -0,0 +1,28 @@
+for i,v in pairs(game.Workspace:GetChildren()) do
+    if v:IsA("Part") and v.Name == "<3" then
+        v:Destroy()
+    end
+end
+
+
+local FarmBase = Instance.new("Part")
+
+FarmBase.Name = "<3"
+FarmBase.Anchored = true
+FarmBase.CanCollide = true
+FarmBase.Transparency = 0.5
+FarmBase.Parent = game:GetService("Workspace")
+FarmBase.Size = Vector3.new(25, 2.8, 25)
+FarmBase.Position = Vector3.new(50, 15000, 50)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = FarmBase.CFrame
+
+while _G.OrbFarm == true do
+    wait()
+    for i,v in pairs(game:GetService("Workspace").Orbs:GetChildren()) do
+        if v:IsA("Part") then
+            v.CanCollide = false
+            v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+end
